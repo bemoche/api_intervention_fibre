f2e63cac3949827936ad832ad4cae9f21495674a
Cherif Bekhtari <bemoche@Cherifs-MacBook-Air.local>
2020-10-27 23:42:34 +0100

    make it better

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.use(function(err,req,res,next){
 });
 
 //lisen for requests
-app.listen(process.env.port || 5503,function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('listening for requests');
 });
