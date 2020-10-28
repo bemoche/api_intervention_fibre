a428e11474c94cae0074a76feffe48919b64a53f
Cherif Bekhtari <bemoche@Cherifs-MacBook-Air.local>
2020-10-28 16:37:39 +0100

    make it better

--- models/interv.js
+++ models/interv.js
@@ -3,31 +3,52 @@ const Schema = mongoose.Schema;
 
 //create interv Schema & model
 const IntervSchema = new Schema({
-    rc: Number,
-    email: String,
-    echec: Boolean,
-    operateur: String,
-    grille: String,
+    rc: {
+        "type": Number,
+        "required": true},
+    email: {
+        "type": String,
+        "required": true},
+    echec: {
+        "type":Boolean,
+        "required":true},
+    operateur: {
+        "type": String,
+        "required": true},
+    grille: {
+        "type": String,
+        "required": true},
     code_postal: Number,
-    rdv_date: String,
-    rdv_heur: String,
+    rdv_date: {
+        "type": String,
+        "required": true},
+    rdv_heur: {
+        "type": String,
+        "required": true},
     client_present: Boolean,
-    hotline: Boolean,
+    hotline: {
+        "type": Boolean,
+        "required": true},
     pm: String,
     pbo: {
         "type": String,
+        "required": true,
         "enum": ["Poteau", "Façade", "Chambre", "Immeuble"]},
     desserte: {
         "type": String,
+        "required": true,
         "enum": ["Aérien", "Façade", "Souterrain", "Aéro-Souterrain", "Fourreau", "Apparent"]},
     pto: {
         "type": String,
+        "required": true,
         "enum": ["Oui", "Non", "Oui/Non"]},
     box_envoi: {
         "type": String,
+        "required": true,
         "enum": ["Reçu", "PasReçu", "????"]},
     box_etat: {
         "type": String,
+        "required": true,
         "enum": ["OK", "NOK"]},
     decharge: String,
     commentaire: String
